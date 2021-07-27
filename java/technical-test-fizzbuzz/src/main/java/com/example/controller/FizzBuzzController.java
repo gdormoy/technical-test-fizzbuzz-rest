49e8535457c6bc3d5470dfc7ce6cf1b761a50476
gdormoy2 <dormoy.guillaume@gmail.com>
2021-07-27 10:41:37 +0200

    make endpoint parameters optional

--- java/technical-test-fizzbuzz/src/main/java/com/example/controller/FizzBuzzController.java
+++ java/technical-test-fizzbuzz/src/main/java/com/example/controller/FizzBuzzController.java
@@ -9,23 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class FizzBuzzController {
     private static final Logger logger = LoggerFactory.getLogger(FizzBuzzController.class);
 
-    // Endpoint fizzbuzz require all params
+    // Endpoint fizzbuzz with optional parameters
     @RequestMapping(value = "/fizzbuzz", method = RequestMethod.GET)
-    public List<String> fizzbuzz(@RequestParam Integer int1, Integer int2, Integer limit, String str1, String str2)
+    public List<String> fizzbuzz(@RequestParam Optional<Integer> int1, Optional<Integer> int2, Optional<Integer> limit, Optional<String> str1, Optional<String> str2)
     {
         List<String> result = new ArrayList<>();
-        for (Integer i = 1; i <= limit; i++) {
-            if (isMultiple(i, int1) && isMultiple(i, int2)){
-                result.add(str1 + str2);
-            } else if (isMultiple(i, int1)) {
-                result.add(str1);
-            } else if (isMultiple(i, int2)) {
-                result.add(str2);
+        for (Integer i = 1; i <= limit.orElseGet(() -> 100); i++) {
+            if (isMultiple(i, int1.orElseGet(() -> 3)) && isMultiple(i, int2.orElseGet(() -> 5))){
+                result.add(str1.orElseGet(() -> "fizz") + str2.orElseGet(() -> "buzz"));
+            } else if (isMultiple(i, int1.orElseGet(() -> 3))) {
+                result.add(str1.orElseGet(() -> "fizz"));
+            } else if (isMultiple(i, int2.orElseGet(() -> 5))) {
+                result.add(str2.orElseGet(() -> "buzz"));
             } else {
                 result.add(String.valueOf(i));
             }
